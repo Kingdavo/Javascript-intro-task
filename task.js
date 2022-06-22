0c2161f637e90f21bb739f6563d3b98a4ffdc0fb
Onuh David Adejoh <104995597+Kingdavo@users.noreply.github.com>
2022-06-22 12:09:16 +0100

    first commit

---
+++ task.js
@@ -0,0 +1,11 @@
+var lastName = "Onuh";
+var firstName = "David";
+var middleName = "Adejoh";
+var height = "1.75m";
+var country = "Nigeria";
+
+name = lastName + " " + firstName + " " + middleName;
+
+document.write("Name:" + "" + name + "<br/>");
+document.write("Height:" + " " + height + "<br>");
+document.write("Country:" + " " + country);
